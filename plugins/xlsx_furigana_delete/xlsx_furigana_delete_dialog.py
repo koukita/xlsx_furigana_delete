2ce0edf6048d3c7f218dcb794e91523a1b452622
koukita <kou35tochas@gmail.com>
2026-02-11 12:50:23 +0900

    ラベルに注意書き

--- plugins/xlsx_furigana_delete/xlsx_furigana_delete_dialog.py
+++ plugins/xlsx_furigana_delete/xlsx_furigana_delete_dialog.py
@@ -8,6 +8,9 @@ class XlsxFuriganaDeleteDialog(QtWidgets.QDialog):
 
         self.setWindowTitle("Excel ふりがな削除")
 
+        # 注意書きラベル
+        w_label = QtWidgets.QLabel("注意！ \n テーブル結合、リレーションを行っている場合は解除されます。 \n 別のプロジェクトで実行してください。")
+
         # レイヤ選択ラベル
         label = QtWidgets.QLabel("対象レイヤを選択：")
 
@@ -24,6 +27,7 @@ class XlsxFuriganaDeleteDialog(QtWidgets.QDialog):
 
         # レイアウト作成
         layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(w_label)
         layout.addWidget(label)
         layout.addWidget(self.layer_combo)
         layout.addWidget(self.buttonBox)
